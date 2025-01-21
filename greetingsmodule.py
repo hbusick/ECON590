457dde1d25017c820eb45091d7119cdb7ac5a621
Hunter Busick <hbusick@unc.edu>
2025-01-21 11:44:59 -0500

    Add files via upload

---
+++ greetingsmodule.py
@@ -0,0 +1,14 @@
+def say_hello(name = None):
+    if name == None:
+        print(f"Hello!")
+    else:
+        print(f"Hello, {name}!")
+
+
+
+def say_bye(name = None):
+    if name == None:
+        print(f"Bye!")
+    else:
+        print(f"Bye, {name}!")
+    
